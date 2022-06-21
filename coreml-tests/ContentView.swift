3f265618b4ba29305cab811d694912239669dc38
Todd Rylaarsdam <todd@toddr.org>
2022-06-21 15:30:14 -0500

    updates

--- coreml-tests/ContentView.swift
+++ coreml-tests/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Run model") {
+            runModelOnImage()
+        }
+    }
+    
+    func runModelOnImage() {
+        do {
+            let model = try VNCoreMLModel(for: MobileNet().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: myResultsMethod)
+            let handler = VNImageRequestHandler(url: URL(string: "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200")!)
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func myResultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation]
+            else { fatalError("huh") }
+        for classification in results {
+            print(classification.identifier, // the scene label
+                  classification.confidence)
+        }
+
     }
 }
 
---
+++ coreml-tests/CoreMLTests.swift
@@ -0,0 +1,17 @@
+//
+//  coreml_testsApp.swift
+//  coreml-tests
+//
+//  Created by Todd Rylaarsdam on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLTests: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
