27f3071cc851bc853f60cc761b680338fea841f3
Todd Rylaarsdam <todd@toddr.org>
2022-06-21 15:15:23 -0500

    updates

---
+++ coreml-tests/coreml_testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreml_testsApp.swift
+//  coreml-tests
+//
+//  Created by Todd Rylaarsdam on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct coreml_testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
