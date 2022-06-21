8ad3543e0c53c5ebadda87b74c59b63092d8bd76
Todd Rylaarsdam <todd@toddr.org>
2022-06-21 15:49:55 -0500

    updates

--- coreml-tests/ContentView.swift
+++ coreml-tests/ContentView.swift
@@ -10,10 +10,49 @@ import CoreML
 import Vision
 
 struct ContentView: View {
+    @State var wakeup = Date()
+    @State var sleepAmount = 4.0
+    @State var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     var body: some View {
-        Button("Run model") {
-            runModelOnImage()
+        VStack {
+            Button("Run model on image") {
+                runModelOnImage()
+            }
+            DatePicker("Wakeup", selection: $wakeup, displayedComponents: .hourAndMinute)
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            Button("Run sleep model") {
+                runModelOnSleep()
+            }
+        }.alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK") { }
+        } message: {
+            Text(alertMessage)
+        }.padding()
+    }
+    
+    func runModelOnSleep() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeup)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeup - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            print(error)
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
+        showingAlert = true
     }
     
     func runModelOnImage() {
